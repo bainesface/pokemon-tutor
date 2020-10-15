22c04cdde9ae71d0de2fcd8d26a178998749a21f
Sarah <sarah.baines@northcoders.com>
2020-10-15 12:32:30 +0100

    pokemon and methods

--- index.js
+++ index.js
@@ -8,11 +8,21 @@
 //fire > grass
 //water > fire
 //grass > water
-function Pokemon(name, sound, move) {
+function Pokemon(name, sound, move, attackDamage, type = 'normal') {
 this.name = name
 this.hitPoints = 100
 this.sound = sound
 this.move =  move
+this.attackDamage = attackDamage
+this.type = type
+}
+
+Pokemon.prototype.talk = function() {
+    return this.sound
+}
+
+Pokemon.prototype.useYourMoves = function() {
+    return this.move
 }
 
 module.exports = Pokemon
--- index.test.js
+++ index.test.js
@@ -22,8 +22,34 @@ describe('Pokemon', () => {
         const charmander =  new Pokemon('charmander', 'fire blast', 'burn')
         expect(charmander.move).toBe('burn')
      })
-     it('instanciated object has a property of move, initialised to a passed move argument', () => {
-        const charmander =  new Pokemon('charmander', 'fire blast', 'burn')
-        expect(charmander.move).toBe('burn')
+     it('instanciated object has a property of attackDamage, initialised to an attackDamage argument', () => {
+        const charmander =  new Pokemon('charmander', 'fire blast', 'burn', 10)
+        expect(charmander.attackDamage).toBe(10)
      })
+     it('instanciated object has a property of type, initialised to a default of normal but able to be reassigned', () => {
+        const pidgy = new Pokemon('pidgy', 'sqwark', 'peck', 8)
+        const charmander =  new Pokemon('charmander', 'fire blast', 'burn', 10, 'fire')
+        expect(pidgy.type).toBe('normal')
+        expect(charmander.type).toBe('fire')
+     })
+});
+
+describe('methods', () => {
+    it('a method called talk is available', () => {
+        const charmander =  new Pokemon('charmander', 'fire blast', 'burn', 10, 'fire')
+        const charmanderMethods = Object.getPrototypeOf(charmander)
+        expect(typeof charmanderMethods.talk).toBe('function')
+    });
+    it('method talk returns the sound of charmander', () => {
+        const charmander =  new Pokemon('charmander', 'fire blast', 'burn', 10, 'fire')
+        expect(charmander.talk()).toBe('fire blast')
+    });
+    it('a method called useYourMoves is available', () => {
+        const charmander =  new Pokemon('charmander', 'fire blast', 'burn', 10, 'fire')
+        expect(typeof charmander.useYourMoves).toBe('function')
+    })
+    it('method useYourMoves returns charmander\'s fave move', () => {
+        const charmander =  new Pokemon('charmander', 'fire blast', 'burn', 10, 'fire')
+        expect(charmander.useYourMoves()).toBe('burn')
+    });
 });
